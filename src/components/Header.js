37b116aa9b5ea187f54ed8af873a6bcfada7f0d8
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-11-08 16:59:07 +0000

    Profile icon added, css tweaked more, more to finish tomorrow!

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,21 @@ const Header = () => {
             <img id="banner" alt = "header" src ={require("../assets/banner.png")}></img>
 
             <div class="navbar">
-                <a href="index.html">Home</a>
                 <div class="dropdown">
                     <button class="dropbtn">PLACEHOLDER</button>
                         <div class="dropdown-content">
                            <p>PLACEHOLDER</p>
                         </div>
                 </div> 
+                <div id="rightside">
+                    <p id = "loggeduser">Welcome USER</p>
+                    <div class="dropdown">
+                    <img alt = "profile" id ="profile" src ={require("../assets/profile.png")} />
+                        <div class="dropdown-content">
+                           <p>PLACEHOLDER</p>
+                        </div>
+                </div>
+                </div>
             </div>
         </div>
     )

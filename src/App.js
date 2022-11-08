59fa90490bedef5e7334caa98a572a07c87f508d
Chris-Basnett <basnett_c@hotmail.co.uk>
2022-11-08 16:46:26 +0000

    created gamebody.js component and some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
+import GameBody from "./components/GameBody"
 import './App.css';
+import './componentstyles/GameBody.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <GameBody />
     </div>
   );
 }

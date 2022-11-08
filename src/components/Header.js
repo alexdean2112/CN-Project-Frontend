06ea810f1eeff00c75b840db9838d3071ea71919
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-11-08 16:17:57 +0000

    Added navbar with basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-
-
+import "../componentstyles/Header.css"
 
 const Header = () => {
     return (
         <div id = "header">
             <img alt = "header" src ={require("../assets/banner.png")}></img>
+
+            <div class="navbar">
+                <a href="index.html">Home</a>
+                <div class="dropdown">
+                    <button class="dropbtn">PLACEHOLDER</button>
+                        <div class="dropdown-content">
+                           <p>PLACEHOLDER</p>
+                        </div>
+                </div> 
+            </div>
         </div>
     )
 }

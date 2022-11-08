3ec1460d59f625ac8e481b61b1259b525d22e072
Scott Balfour <scottbalf@gmail.com>
2022-11-08 12:11:25 +0000

    inital footer

---
+++ src/components/footer.js
@@ -0,0 +1,46 @@
+import twitLogob from '../components/images'
+import faceLogob from '../components/images'
+import instaLogob from '../components/images'
+
+const BeesFooter = () => {
+    return (
+        <div>
+            <div className="BeesFooter">
+                <div className="FooterCards">
+                <div className="FooterLinks">
+                    <div className="BeesLinksTitle"><u><strong>The Bee's Keys</strong></u></div>
+                    <div className="BeesLinks2">
+                    <div>About Us</div>
+                    <div>Why The Bee's Knees?</div>
+                    <div>Discord</div>
+                    <div>Careers</div>
+                    </div>
+                </div>
+                <div className="BeesServices">
+                    <div className="BeesHelpTitle"><u><strong>Cat Services</strong></u></div>
+                    <div className="BeesHelp2">
+                    <div>API</div>
+                    <div>Sell on The Bee's Knees</div>
+                    <div>FAQ</div>
+                    </div>
+                </div>
+                <div className="BeesUser">
+                    <div className="BeesUserTitle"><u><strong>Information</strong></u></div>
+                    <div className="BeesUser2">
+                    <div>T's and C's</div>
+                    <div>Privacy Policy</div>
+                    <div>Contact</div>
+                    </div>
+                </div>
+                <div className="BeesSocials">
+                <div className="twitLogo"><img src={twitLogob} alt="twitter logo"></img></div>
+                <div className="faceLogo"><img src={faceLogob} alt="facebook logo"></img></div>
+                <div className="instaLogo"><img src={instaLogob} alt="instagram logo"></img></div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BeesFooter;

a402bbd0b4c36d9e61f13ad5e6184269461308c8
horsellcommon <crawfordj716@gmail.com>
2022-11-09 12:11:47 +0000

    Basic CSS styling and Checkout section

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,23 @@
+import "../componentstyles/Checkout.css";
+
 const Checkout = () => {
   return (
     <div>
       <h1>YOUR SHOPPING CART</h1>
-      <div id="outer-box">
+      <div className="outer-box">
         <div className="inner-box-upper">
-            <img src="" alt="" height="50" width="50" />
-            <p>placeholder game title</p>
+          <img src="" alt="gameimage" height="50" width="50" />
+          <p>placeholder game title</p>
+          <img src="" alt="platformimage" height="16" width="16" />
+          <p>£19.99</p>
+        </div>
+        <div className="inner-box-lower">
+          <div className="top-text">
+            <h4>Estimated total</h4>
+            <h4>£19.99</h4>
+          </div>
+          <p id="center">To finalise your order, select purchase below.</p>
+          <div id="purchase"><button id="padded-button">Purchase item(s)</button></div>
         </div>
       </div>
     </div>

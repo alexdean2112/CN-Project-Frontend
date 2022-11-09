414ce943b72d52c7f44fdbc0f236108f052ec868
Scott Balfour <scottbalf@gmail.com>
2022-11-09 15:32:34 +0000

    implementing basket

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import Footer from "./components/Footer";
 import Basket from "./components/Basket";
 import "./App.css";
 import "./componentstyles/GameBody.css";
+import { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
       <Header />
+      <Basket show={show} />
+      <StyledButton onClick={() => setShow(!show)}>Show/Hide Cart</StyledButton>
       <GameBody />
       <Mainpage />
-      <Basket />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+const StyledButton = styled.button`
+  border: none;
+  height: 50px;
+  width: 100px;
+  background-color: tan;
+  color: white;
+  border-radius: 5px;
+  margin: 10px;
+  margin-right: 15px;
+  cursor: pointer;
+`;

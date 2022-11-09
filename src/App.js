29a23dd693c437142de25add6c01e147cc87ede2
horsellcommon <crawfordj716@gmail.com>
2022-11-09 11:52:37 +0000

    Basic layout for Checkout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 /* Comments are all required code for register/login*/
 
 // import {Register, Login} from "./components/Register";
-import Header from "./components/Header";
-import GameBody from "./components/GameBody";
-import Mainpage from "./components/Mainpage";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import GameBody from "./components/GameBody";
+// import Mainpage from "./components/Mainpage";
+// import Footer from "./components/Footer";
+import Checkout from "./components/Checkout";
 import "./App.css";
 import "./componentstyles/GameBody.css";
 import { useState } from "react";
@@ -13,12 +14,13 @@ function App() {
   // const [user, setUser] = useState();
   return (
     <div className="App">
+      <Checkout />
       {/* <Register setter={setUser} />
       <Login setter={setUser} /> */}
-      <Header />
+      {/* <Header />
       <GameBody />
       <Mainpage />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Checkout.js
@@ -0,0 +1,15 @@
+const Checkout = () => {
+  return (
+    <div>
+      <h1>YOUR SHOPPING CART</h1>
+      <div id="outer-box">
+        <div className="inner-box-upper">
+            <img src="" alt="" height="50" width="50" />
+            <p>placeholder game title</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;

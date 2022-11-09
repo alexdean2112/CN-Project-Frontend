e334119bbcc1c73bca72de59bf656f95c872408e
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-11-09 14:24:03 +0000

    completed most styling and basic setup for card, add to basket button added but needs further styling

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -8,6 +8,16 @@ const GameCard = () => {
                 <img  id = "gamecover" alt="gamecover" src = {GodOfWar}></img>
             </div>
 
+            <div className = "gamedetails">
+                <p id = "gametitle">GAMETITLEHERE</p>
+                <p id = "gameplatforms">GAMEPLATFORMSHERE</p>
+                <p id = "gamesummary">GAMESUMMARYHERE</p>
+                <div className = "pricesection">
+                    <p id = "gameprice">PRICEHERE</p>
+                    <button id = "addtobasket">Add To Basket</button>
+                </div>
+            </div>
+
 
         </div>
 

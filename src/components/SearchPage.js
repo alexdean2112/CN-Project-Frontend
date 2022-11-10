ee79b0b6fd64887b1e9e9f536269f908bb5e20ed
Reyan19k <dinreyanud@gmail.com>
2022-11-10 15:33:49 +0000

    added basket to search page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import SearchBody from "./SearchBody"
+import Basket from "./Basket"
 
 const SearchPage = () => {
     return (
         <div>
             <Header />
+            <Basket />
             <SearchBody />
             <Footer />
         </div>

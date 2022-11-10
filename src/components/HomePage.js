b9d9e57c404945a07388d0242610a49e37d936f1
Reyan19k <dinreyanud@gmail.com>
2022-11-10 15:31:07 +0000

    added basket to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Mainpage from "./Mainpage"
+import Basket from "./Basket"
+
 
 const HomePage = () => {
     return (
         <div>
             <Header />
+            <Basket />
             <Mainpage />
             <Footer />
         </div>

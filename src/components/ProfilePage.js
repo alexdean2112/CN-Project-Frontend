517cbec98c943c1db2b3bdaf80123d414be16628
Reyan19k <dinreyanud@gmail.com>
2022-11-10 15:35:08 +0000

    added basket to profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Profile from "./Profile"
+import Basket from "./Basket"
 
 const ProfilePage = () => {
     return (
         <div>
             <Header />
+            <Basket />
             <Profile />
             <Footer />
         </div>

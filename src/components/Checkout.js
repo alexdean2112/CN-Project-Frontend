25be5fcea2e36c7f5ed5fe5a7569e2180c606f55
horsellcommon <crawfordj716@gmail.com>
2022-11-15 16:15:29 +0000

    Even dicier total calculator

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,38 +2,59 @@ import "../componentstyles/Checkout.css";
 import { createOrder } from "../utils/orderUtils";
 
 const Checkout = ({ basket, atb, user }) => {
+  const prices = [];
+
+  const addUp = () => {
+    const sum = prices.reduce((partialSum, a) => partialSum + a, 0)
+    return sum;
+  };
 
   const checkoutHandler = () => {
     for (let i = 0; i < basket.length; i++) {
-      createOrder(basket[i].title, basket[i].price, user)
-    };
+      createOrder(basket[i].title, basket[i].price, user);
+    }
     atb([]);
   };
+
+  const newPrice = (item) => {
+    const newTotal = total + item;
+    setTotal(newTotal);
+  }
+
+  useEffect(() => {
+    basket.map((item, index) => (
+      newPrice(item.price)
+    ))
+  }, [basket])
 
   return (
     <div>
       <h1>YOUR SHOPPING CART</h1>
       <div className="outer-box">
-
-        {basket.map((item, index) => (
-          <div className="inner-box-upper">
-            <img src="" alt="gameimage" height="50" width="50" />
-            <p>{item.title}</p>
-            <div id="vert-line" />
-            <img src="" alt="platformimage" height="16" width="16" />
-            <p>£{item.price}</p>
-          </div>
-        ))}
+        {basket.map((item, index) => {
+          prices.push(parseInt(item.price));
+          return (
+            <div className="inner-box-upper">
+              <img src="" alt="gameimage" height="50" width="50" />
+              <p>{item.title}</p>
+              <div id="vert-line" />
+              <img src="" alt="platformimage" height="16" width="16" />
+              <p>£{item.price}</p>
+            </div>
+          );
+        })}
 
         <div id="thin-line" />
         <div className="inner-box-lower">
           <div className="top-text">
             <h4>Estimated total</h4>
-            <h4>£{total}</h4>
+            <h4>£{addUp()}.00</h4>
           </div>
           <p id="center">To finalise your order, select checkout below.</p>
           <div id="purchase">
-            <button id="padded-button" onClick={checkoutHandler}>CHECKOUT</button>
+            <button id="padded-button" onClick={checkoutHandler}>
+              CHECKOUT
+            </button>
           </div>
         </div>
       </div>
